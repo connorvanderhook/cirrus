22b9c970656a3aba80e2b129cf0bf2c171064e29
Connor Vanderhook <connor@trueanthem.com>
2019-05-10 18:03:03 +1000

    fix: comment typo

--- api.go
+++ api.go
@@ -3,7 +3,7 @@ package cirrus
 import "io"
 
 // FileStore provides an interface for file management
-// across the diferent file store api providers
+// across the different file store api providers
 type FileStore interface {
 	// Save saves data from the io.Reader to a file named by path
 	Save(path string, r io.Reader) error
